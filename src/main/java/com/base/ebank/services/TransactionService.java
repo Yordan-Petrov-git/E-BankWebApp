8f08e206dc2efe59639c178ba80552c999eb1fb9
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-10 00:08:45 +0300

    transaction log implementation

--- src/main/java/com/base/ebank/services/TransactionService.java
+++ src/main/java/com/base/ebank/services/TransactionService.java
@@ -1,4 +1,20 @@
 package com.base.ebank.services;
 
+import com.base.ebank.entities.Transaction;
+import com.base.ebank.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TransactionService {
+    private final TransactionRepository transactionRepository;
+@Autowired
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+    public List<Transaction> getAllByName (String name){
+
+    }
 }
