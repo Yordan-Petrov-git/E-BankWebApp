4880c9e6b6e95c2768d1786590125fe13147e4a4
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-09 03:38:09 +0300

    create bank account

--- src/main/java/com/base/ebank/controllers/BankAccountController.java
+++ src/main/java/com/base/ebank/controllers/BankAccountController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -21,12 +22,27 @@ public class BankAccountController {
         this.bankAccountService = bankAccountService;
     }
 
-    @GetMapping("/create")
+        @GetMapping("/create")
+        @PreAuthorize("isAuthenticated()")
+        public String createBankAccount(Model model, @ModelAttribute("bankAccountBindingModel")BankAccountBindingModel bankAccountBindingModel, Principal principal){
+    bankAccountBindingModel.setUsername(principal.getName());
+    model.addAttribute("view","accounts/create-account");
+    model.addAttribute("bankAccountBindingModel",bankAccountBindingModel);
+    return "fragments/layout";
+        }
+
+
+    @PostMapping("/create")
     @PreAuthorize("isAuthenticated()")
-    public String createBankAccount(Model model, @ModelAttribute("bankAccountBindingModel")BankAccountBindingModel bankAccountBindingModel, Principal principal){
-bankAccountBindingModel.setUsername(principal.getName());
-model.addAttribute("view","accounts/create-account");
-model.addAttribute("bankAccountBindingModel",bankAccountBindingModel);
-return "fragments/layout";
+    public String createBankAccountConfirm(Model model, @ModelAttribute("bankAccountBindingModel")BankAccountBindingModel bankAccountBindingModel){
+
+    if(!this.bankAccountService.createAccount(bankAccountBindingModel)){
+
+        model.addAttribute("view","accounts/create-account");
+        model.addAttribute("bankAccountBindingModel",bankAccountBindingModel);
+        return "fragments/layout";
+    }
+
+        return "redirect:/home";
     }
 }
