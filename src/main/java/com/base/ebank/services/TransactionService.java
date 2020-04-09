2ca323933e36fa6ab1ebae951c0fb01be8f35503
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-10 00:14:54 +0300

    transaction log implementation

--- src/main/java/com/base/ebank/services/TransactionService.java
+++ src/main/java/com/base/ebank/services/TransactionService.java
@@ -5,7 +5,10 @@ import com.base.ebank.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionService {
@@ -14,7 +17,10 @@ public class TransactionService {
     public TransactionService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
     }
-    public List<Transaction> getAllByName (String name){
-
+    public Set<Transaction> getAllByName (String name){
+      return    this.transactionRepository.findAll().stream().filter(t ->t.getToAccount().getOwner().getUsername().equals(name) ||
+                 t.getFromAccount().getOwner().getUsername().equals(name))
+              .sorted(Comparator.comparing(Transaction::getType))
+                 .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
