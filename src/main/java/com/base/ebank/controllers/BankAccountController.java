ba1860f980019e3a32c500cfc04bffdfdc8d2fe8
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-04-09 03:33:41 +0300

    create bank account

---
+++ src/main/java/com/base/ebank/controllers/BankAccountController.java
@@ -0,0 +1,32 @@
+package com.base.ebank.controllers;
+
+import com.base.ebank.bindingModels.BankAccountBindingModel;
+import com.base.ebank.services.BankAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/accounts")
+public class BankAccountController {
+      private  final BankAccountService bankAccountService;
+@Autowired
+    public BankAccountController(BankAccountService bankAccountService) {
+        this.bankAccountService = bankAccountService;
+    }
+
+    @GetMapping("/create")
+    @PreAuthorize("isAuthenticated()")
+    public String createBankAccount(Model model, @ModelAttribute("bankAccountBindingModel")BankAccountBindingModel bankAccountBindingModel, Principal principal){
+bankAccountBindingModel.setUsername(principal.getName());
+model.addAttribute("view","accounts/create-account");
+model.addAttribute("bankAccountBindingModel",bankAccountBindingModel);
+return "fragments/layout";
+    }
+}
